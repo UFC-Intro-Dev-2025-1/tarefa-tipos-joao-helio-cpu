12db3cf1298ce0807db6a27981579d828379cd21
joaohelioss <118625651+joao-helio-cpu@users.noreply.github.com>
2025-05-22 20:15:31 +0000

    tarefaFeita

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-// Escreva seu código aqui!
+//atv
+let computador: Number = 25;
+let corSala: String = 'Branca';
+let luzLigada: Boolean = true;
+let notaFinal: undefined;
+
+interface Projetor {
+    marca: string;
+    quantidade: number;
+}
+
+let projetor: Projetor = {
+    marca: 'acer',
+    quantidade: 1,
+};
+
+ console.log('A sala tem ' + computador +' computadores');
+ console.log('A cor da sala é ' + corSala);
+ console.log('A luz da sala está ligada ' +luzLigada);
+ console.log('A nota da sala está ' + notaFinal);
+ console.log('O projetor é da marca '+ projetor['marca'] +' e na sala tem '+projetor['quantidade']);
+ 
